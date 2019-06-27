608676939ecaab9eb54f5d4d152b7c2e0148daad
Bernard Lee <38368050+tejaboy@users.noreply.github.com>
2019-06-27 11:13:27 +0800

    Added motor movement.

--- Client/safe.py
+++ Client/safe.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 from ev3dev.ev3 import *
+from ev3dev2.motor import *
 
 import paho.mqtt.client as mqtt
 import sys
 
 def main():
+    setupMQTT()
+    setupEV3()
+
+## EV3
+def setupEV3():
+	# We will do all the initialization of motors and sensor here.
+	# For this practical, there are only one motor.
+	motor = LargeMotor()
+
+def open_safe():
+	motor.on_for_seconds(100, 1.2)
+
+## MQTT
+def setupMQTT():
     client = mqtt.Client()
     client.connect("test.mosquitto.org", 1883, 60)
 
@@ -23,7 +38,7 @@ def on_message(client, userdata, msg):
     if msg.payload.decode() == "unlock":
         debug_print("Unlocking ...")
 
-        # TODO: Motor movement
+        open_safe()
 
 def debug_print(*args, **kwargs):
 	'''Print debug messages to stderr.
